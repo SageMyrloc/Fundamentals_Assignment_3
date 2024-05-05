1a9503f7df7bcbdf357e338d61b43cdfdea7755f
sagemyrloc <x9i27@students.keele.ac.uk>
2024-05-05 19:12:07 +0100

    finished Visualisation

--- static/js/creative.js
+++ static/js/creative.js
@@ -75,7 +75,7 @@ function createSVG(container) {
 
 function clearSVG(svg) {
   // Remove all elements within the SVG
-  svg.selectAll("*").remove();
+  d3.select("svg").remove();
 }
 
 function stackedBarPlot() {
@@ -88,6 +88,7 @@ function stackedBarPlot() {
     svg = createSVG(svgContainer);
   } else {
     clearSVG(svg);
+    svg = createSVG(svgContainer);
   }
 
   // extract the data
@@ -158,7 +159,8 @@ function stackedBarPlot() {
   const baroffset = 60;
 
   svg
-    .selectAll(".stack")
+    .append("g")
+    .selectAll("g")
     .data(stackedSeries)
     .enter()
     .append("g")
@@ -175,9 +177,11 @@ function stackedBarPlot() {
     .style("stroke", "black")
     .style("stroke-width", 1);
 
-  //add a legend
   // Add legend
-  var legend = svg.append("g").attr("transform", "translate(" + (containerWidth - 500) + "," + 0 + ")");
+  var legend = svg
+    .append("g")
+    .attr("class", "legend")
+    .attr("transform", "translate(" + (containerWidth - 500) + "," + 0 + ")");
 
   // Create legend items
   keys.forEach(function (key, i) {
@@ -192,6 +196,15 @@ function stackedBarPlot() {
       .attr("dy", ".35em")
       .text(key.charAt(0).toUpperCase() + key.slice(1));
   });
+
+   // finally add a title to the graph
+   svg.append("text")
+   .attr("x", (containerWidth * 0.35 ))             
+   .attr("y", 0)
+   .attr("text-anchor", "middle")  
+   .style("font-size", "30px") 
+   .style("text-decoration", "bold")  
+   .text("Stacked Graph showing review type sorted by Airline");
 }
 
 function groupedBarPlot() {
@@ -202,16 +215,114 @@ function groupedBarPlot() {
     svg = createSVG(svgContainer);
   } else {
     clearSVG(svg);
+    svg = createSVG(svgContainer);
   }
-  d3.csv("/static/data/Kaggle_TwitterUSAirlineSentiment.csv", function (error, data) {
-    if (error) throw error;
-    // Preprocess the data
-    var airlines = d3
-      .map(data, function (d) {
-        return d.airline;
-      })
-      .keys();
+
+  // extract the data
+  const airlineNames = airlineSentiment.map((airlineObj) => airlineObj.airline);
+  const negativeReasons = Array.from(new Set(airlineSentiment.flatMap((d) => Object.keys(d.negativeReasons))));
+
+  // get the container width for sizing the graph
+  var containerWidth = parseInt(svgContainer.style("width"), 10);
+  var containerHeight = parseInt(svgContainer.style("height"), 10);
+
+  // Create the Y axis
+  var yScale = d3
+    .scaleLinear()
+    .domain([0, 15])
+    .range([containerHeight - 150, 0]);
+
+  // add the Y axis
+  svg
+    .append("g")
+    .attr("transform", "translate(5" + 0 + ")")
+    .call(d3.axisLeft(yScale))
+    .style("font-size", "18px");
+
+  // add Y label
+  svg.append("text").attr("x", -400).attr("y", 0).attr("text-anchor", "middle").attr("transform", "rotate(-90)").text("Number of Reviews").style("font-size", "30px");
+
+  // create the x axis
+  var xScaleAirline = d3
+    .scaleBand()
+    .range([0, containerWidth * 0.6])
+    .domain(airlineNames)
+    .padding(0.2);
+
+  // define the x-scale for reasons
+  var xScaleReasons = d3.scaleBand().range([0, xScaleAirline.bandwidth()]).domain(negativeReasons).padding(0.05);
+
+  // add the x axis
+  svg
+    .append("g")
+    .attr("class", "x-axis")
+    .attr("transform", "translate(50," + (containerHeight - 150) + ")")
+    .call(d3.axisBottom(xScaleAirline))
+    .selectAll("text")
+    .style("text-anchor", "middle")
+    .style("font-size", "18px");
+
+  // colour scheme for the bars
+  const colourScale = d3.scaleOrdinal().domain(negativeReasons).range(d3.schemeCategory10);
+
+  const groupShift = 56;
+  // Render grouped bars for negative reasons
+  svg
+    .selectAll(".group")
+    .data(airlineSentiment)
+    .enter()
+    .append("g")
+    .attr("class", "group")
+    .attr("transform", (d) => `translate(${xScaleAirline(d.airline) + groupShift} , 0)`)
+    .selectAll("rect")
+    .data((d) => negativeReasons.map((reason) => ({ airline: d.airline, reason, count: d.negativeReasons[reason] })))
+    .enter()
+    .append("rect")
+    .attr("x", (d) => xScaleReasons(d.reason))
+    .attr("y", (d) => yScale(d.count))
+    .attr("width", xScaleReasons.bandwidth())
+    .attr("height", (d) => yScale(0) - yScale(d.count))
+    .attr("fill", (d) => colourScale(d.reason))
+    .style("stroke", "black")
+    .style("stroke-width", 1);
+
+  // add the x axis label
+  svg
+    .append("text")
+    .attr("x", 550)
+    .attr("y", containerHeight - 90)
+    .attr("text-anchor", "middle")
+    .text("Airlines")
+    .style("font-size", "30px");
+
+  // Add legend
+  var legend = svg
+    .append("g")
+    .attr("class", "legend")
+    .attr("transform", "translate(" + (containerWidth - 500) + "," + 0 + ")");
+
+  // Create legend items
+  negativeReasons.forEach(function (key, i) {
+    var legendItem = legend.append("g").attr("transform", "translate(0," + i * 20 + ")");
+
+    legendItem.append("rect").attr("width", 20).attr("height", 20).attr("fill", colourScale(key)).style("stroke", "black").style("stroke-width", 1);
+
+    legendItem
+      .append("text")
+      .attr("x", 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .text(key.charAt(0).toUpperCase() + key.slice(1));
   });
+
+  // finally add a title to the graph
+  svg.append("text")
+        .attr("x", (containerWidth * 0.35 ))             
+        .attr("y", 0)
+        .attr("text-anchor", "middle")  
+        .style("font-size", "30px") 
+        .style("text-decoration", "bold")  
+        .text("Reasons for negative reviews grouped by Airline");
 }
 
 document.addEventListener("DOMContentLoaded", function () {
