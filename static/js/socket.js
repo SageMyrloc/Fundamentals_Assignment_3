aad393b8d42d524c2d1582896f4cd60bbf70cf92
sagemyrloc <x9i27@students.keele.ac.uk>
2024-05-06 02:00:06 +0100

    socket changes to try and rate limit

--- static/js/socket.js
+++ static/js/socket.js
@@ -1,27 +1,48 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  namespace = "/test";
+  var socket = io(namespace);
 
-    namespace = '/test';
-    var socket = io(namespace);
+  socket.on("connect", function () {
+    socket.emit("my_event", { data: "connected to the SocketServer..." });
+  });
 
-    socket.on('connect', function() {
-        socket.emit('my_event', {data: 'connected to the SocketServer...'});
-    });
+  socket.on("my_response", function (msg, cb) {
+    $("#log").append(
+      "<br>" +
+        $("<div/>")
+          .text("logs #" + msg.count + ": " + msg.data)
+          .html()
+    );
+    if (cb) cb();
+  });
 
-    socket.on('my_response', function(msg, cb) {
-        $('#log').append('<br>' + $('<div/>').text('logs #' + msg.count + ': ' + msg.data).html());
-        if (cb)
-            cb();
-    });
-    $('form#emit').submit(function(event) {
-        socket.emit('my_event', {data: $('#emit_data').val()});
-        return false;
-    });
-    $('form#broadcast').submit(function(event) {
-        socket.emit('my_broadcast_event', {data: $('#broadcast_data').val()});
-        return false;
-    });
-    $('form#disconnect').submit(function(event) {
-        socket.emit('disconnect_request');
-        return false;
-    });
+  // Function to emit data with rate limiting
+  function emitWithRateLimit(data) {
+    var now = Date.now();
+    if (now - lastEmitTime >= rateLimitInterval) {
+      socket.emit("my_event", { data: data });
+      lastEmitTime = now;
+    } else {
+      console.log("Rate limit exceeded. Ignoring emit request.");
+    }
+  }
+
+  // Handle form submission with rate-limited emit
+  $("form#emit").submit(function (event) {
+    var emitData = $("#emit_data").val();
+    emitWithRateLimit(emitData);
+    return false;
+  });
+
+  // Handle form submission to broadcast with rate limiting
+  $("form#broadcast").submit(function (event) {
+    var broadcastData = $("#broadcast_data").val();
+    emitWithRateLimit(broadcastData);
+    return false;
+  });
+
+  $("form#disconnect").submit(function (event) {
+    socket.emit("disconnect_request");
+    return false;
+  });
 });
